fe9bc6c9b311d77a815005e412decfa7b45bc37b
Yolea Mayers <yoleamayers@Yoleas-Air.fios-router.home>
2020-03-29 14:39:47 -0400

    drafted README

--- second_project/README.md
+++ second_project/README.md
@@ -1,1 +1,26 @@
 ![superNova](https://imgur.com/pxrkv9N.gif);
+
+World COVD-19 Information
+
+MVP GOALS
+
+I am creating an app that provides the total number of COVID-19 cases per Country. There will be homepage listing all of the countries. A user will be able to click on the country of inquiry and information about the COVID-19 cases will display on a separate page. The goal of this app is to keep users well informed, during this pandemic.
+
+I will be using the COVID-19 API (https://covid19api.com/).
+
+COMPONENT HEIRARCHY
+
+APP
+HEADER
+HOME
+COUNTRY
+DETAILED INFO
+FOOTER
+
+Post MVP GOALS
+
+I'd like to integreate the CDC API (https://open.cdc.gov/apis.html) to incorporate more detailed information on COVID-19.
+
+WIREFRAMES
+
+![iPhone]();
--- second_project/src/App.js
+++ second_project/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { Link, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { response: [] };
+  }
+
+  componentDidMount = async () => {
+    const response = await axios.get(
+      `https://api.covid19api.com/summary`
+    );
+
+    this.setState({ response: response.Countries });
+  };
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
 }
 
 export default App;
